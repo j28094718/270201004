e2d5f11fd2ab24b30dd3e1a449df200ecf758abc
j28094718 <haliluyanik@std.iyte.edu.tr>
2020-10-26 09:33:30 +0000

    rvsd

--- lab1/seasons.py
+++ lab1/seasons.py
@@ -1,11 +1,10 @@
 month = int(input("enter the month:"))
 day = int(input("enter the day:"))
-if (month == 3 and day >= 20) or (month == 4) (month ==5) or (month ==6 and day < 21): 
+if (month == 3 and day >= 20) or (3 < month < 6) or (month ==6 and day < 21): 
   print("Spring")
-elif (month == 6 and day >= 21) or (month == 7) or (month == 8) or (month == 9 and day < 22):
+elif (month == 6 and day >= 21) or (6< month <9) or (month == 9 and day < 22):
   print("Summer")
-elif (month == 9 and day >= 22) or (month == 10) or (month == 11) or (month == 12 and day <21): 
+elif (month == 9 and day >= 22) or (9 < month < 12) or (month == 12 and day <21): 
   print("Fall")
-elif (month == 12 and day >= 21) or (month == 1) or (month == 2) or (month == 3 and day < 20): 
+elif (month == 12 and day >= 21) or (12 < month < 3) or (month == 3 and day < 20): 
   print("Winter")
-  
