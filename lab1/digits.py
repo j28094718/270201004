da4fc408a0e97e34fa1fb44a6656544ddc23f199
j28094718 <haliluyanik@std.iyte.edu.tr>
2020-11-09 08:47:54 +0000

    ctups

---
+++ lab1/digits.py
@@ -0,0 +1,8 @@
+number = input("Enter the number:")
+if int(number) // 10 >= 1:
+  sum = int(number[-1]) + int(number[-2])
+  print("sum of last two digits", sum)
+else : 
+  sum = (number[-1])
+  print("0" + sum)
+
