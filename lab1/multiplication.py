8c9eb0d91ff4f3e66debb201da049e3740b83903
j28094718 <haliluyanik@std.iyte.edu.tr>
2020-11-16 08:56:49 +0000

    mtp

---
+++ lab1/multiplication.py
@@ -0,0 +1,3 @@
+number = int(input("number1:"))
+for i in range(1,11): 
+  print(number_1 , "x", i, "=", number_1 * i)
---
+++ lab1/trial.py
@@ -0,0 +1,11 @@
+number = int(input("number:"))
+if number // 3 ==0: 
+  if number // 4 == 0: 
+    print(12)
+  else : 
+    print(3)
+elif number // 4 == 0: 
+  if number // 3 == 0: 
+    print(12)
+  else : 
+    print(4)
